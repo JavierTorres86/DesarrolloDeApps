6a27d29ac622d4916604046e26fc3b08cdf3f3f7
Javier Torres <javiertorres86@outlook.com>
2023-09-03 13:13:34 -0300

    lista optimizada

--- src/components/Style.js
+++ src/components/Style.js
@@ -72,7 +72,7 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
       alignItems: 'center',
       marginTop: 22,
-      backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fondo semitransparente para cubrir la pantalla
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
   
 });
